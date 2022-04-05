de0b644d07925b81e4ac18a65f82efc54fa0e3b3
Michael-Baas <michaelbaas@me.com>
2022-04-05 09:41:53 -0400

    Add keyboard support for calculator

--- main.js
+++ main.js
@@ -12,6 +12,26 @@
 // Take user input from keys on keyboard
 
 
+// Add user input to the primary input calculator screen 
+// Keep track of first number until an operator is pressed 
+// Update the secondary input screen with the first number and operator
+// Update primary input screen with second number until another operator or equals sign is pressed
+// if another non-equals operator is pressed carry out the prior operation then preform the selected operators operation on the calculated value -- essentially run the calculate method no matter what after the first operator is pressed and the second input is entered 
+
+
+
+
+// Declare global variables for different inputs
+const numbers = document.querySelectorAll('[data-number]');
+const operators = document.querySelectorAll('[data-operator]');
+const equals = document.querySelector('[data-equals]');
+const clear = document.querySelector('[data-clear]');
+const backspace = document.querySelector('[data-backspace]');
+
+// global Declare variables for output screens
+const currentOutput = document.querySelector('[data-current-output]')
+const previousOutput = document.querySelector('[data-previous-output]')
+
 // ---------------------------- Calculator Class --------------------------------------
 class Calculator{
     constructor(currentOutput, previousOutput){
@@ -59,27 +79,10 @@ class Calculator{
     }
 }
 
-
-// Declare Calculator Object
-
-
-
-// Declare variables for different inputs
-const numbers = document.querySelectorAll('[data-number]');
-const operators = document.querySelectorAll('[data-operator]');
-const equals = document.querySelector('[data-equals]');
-const clear = document.querySelector('[data-clear]');
-const backspace = document.querySelector('[data-backspace]');
-
-// Declare variables for output screens
-const currentOutput = document.querySelector('[data-current-output]')
-const previousOutput = document.querySelector('[data-previous-output]')
-
 const calc = new Calculator(currentOutput, previousOutput);
 
 // ---------------------------- Click input --------------------------------------
-// Get user inputs from clicks 
-
+// -- Number Buttons -- Collect number that was clicked 
 numbers.forEach(number => {
     number.addEventListener('click', () => {
     // Append current number with what was clicked 
@@ -89,6 +92,7 @@ numbers.forEach(number => {
     })
 })
 
+// -- Operator Buttons -- Collect operator that was clicked 
 operators.forEach(operator => {
     operator.addEventListener('click', () => {
     // Append current operator to previous operator screen
@@ -99,7 +103,7 @@ operators.forEach(operator => {
     })
 })
 
-
+// -- Equals Button -- Calculate total: if repeated more than once in a row, repeat the last operator. 
 equals.addEventListener('click', () => {
     // Execute the previous operands
     // Add the previous operand and an equals sign to the secondary display
@@ -108,6 +112,7 @@ equals.addEventListener('click', () => {
     // Update the display 
 })
 
+// -- Clear Button -- Clear everything from both primary and secondary inputs 
 clear.addEventListener('click', () => {
     // Reset the primary display to zero, clear out the secondary display
     calc.clear();
@@ -115,13 +120,13 @@ clear.addEventListener('click', () => {
     calc.updateDisplay();
 })
 
+// -- Backspace button-- Clear the last digit entered into the primary screen
 backspace.addEventListener('click', () =>  {
     // Backspace the last digit entered in the primary display
     calc.backspace();
     // Update the Display
     calc.updateDisplay();
 })
-
 
 // ---------------------------- Keyboard input --------------------------------------
 // Get user input from the Keyboard
@@ -131,24 +136,44 @@ document.addEventListener('keyup', (e) => {
     console.log(keyValue, codeValue)
     switch(keyValue) {
         case '1' :
+            calc.appendInput(keyValue)
+            calc.updateDisplay();
             break;
         case '2':
+            calc.appendInput(keyValue)
+            calc.updateDisplay();
             break;
         case '3' :
+            calc.appendInput(keyValue)
+            calc.updateDisplay();
             break;
         case '4':
+            calc.appendInput(keyValue)
+            calc.updateDisplay();
             break;
         case '5' :
+            calc.appendInput(keyValue)
+            calc.updateDisplay();
             break;
         case '6':
+            calc.appendInput(keyValue)
+            calc.updateDisplay();
             break;
         case '7' :
+            calc.appendInput(keyValue)
+            calc.updateDisplay();
             break;
         case '8':
+            calc.appendInput(keyValue)
+            calc.updateDisplay();
             break;
         case '9' :
+            calc.appendInput(keyValue)
+            calc.updateDisplay();
             break;
         case '0' :
+            calc.appendInput(keyValue)
+            calc.updateDisplay();
             break;
         case 'Backspace' :
             break;
@@ -172,12 +197,3 @@ document.addEventListener('keyup', (e) => {
 
 
 
-// Add user input to the primary input calculator screen 
-// Keep track of first number until an operator is pressed 
-// Update the secondary input screen with the first number and operator
-// Update primary input screen with second number until another operator or equals sign is pressed
-// if another non-equals operator is pressed carry out the prior operation then preform the selected operators operation on the calculated value -- essentially run the calculate method no matter what after the first operator is pressed and the second input is entered 
-
-
-// -- Clear Button -- Clear everything from both primary and secondary inputs 
-// -- Delete Button-- Clear the last digit entered into the primary screen
